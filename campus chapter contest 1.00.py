3cfe403fe5ce79499317cc8ca54bb306eb77aadb
Nikhil Agarwal <45250158+nikhil24agarwal@users.noreply.github.com>
2020-04-09 22:49:15 +0530

    more question solution added

--- campus chapter contest 1.00.py
+++ campus chapter contest 1.00.py
@@ -243,3 +243,203 @@ try:
 except:
     pass
 
+#a lot of car full
+
+try:
+# if(True):
+    for _ in range(int(input())):
+        m,n=map(int,input().split())
+        c=[]
+        last=0
+        flag=True
+        rowstart=0
+        rowend=0
+        for i in range(m):
+            k=list(input().split())
+            co=0
+            k.append(-1)
+            k.append(-1)
+            for j in range(len(k)):
+                if(k[j]=="P"):
+                    co+=1
+                    if(co==1):
+                        k[-2]=j
+                    k[-1]=j
+                    rowend=i
+                
+            if(k[-1]!=-1 and flag):
+                flag=False
+                rowstart=i
+                if(i%2==0):
+                    last=k[-2]
+                else:
+                    last=k[-1]
+            c.append(k)
+        count=0
+#         print(c)
+        
+        for i in range(rowstart,rowend+1):
+            
+            if(i%2==0 and c[i][-1]!=-1):
+                count+=abs(last-c[i][-2])
+                count+=c[i][-1]-c[i][-2]
+                last=c[i][-1]
+            elif(i%2!=0 and c[i][-1]!=-1):
+                count+=abs(last-c[i][-1])
+                count+=c[i][-1]-c[i][-2]
+                last=c[i][-2]
+            count+=1
+        print(count-1)
+    
+    
+    
+except:
+    pass
+
+
+#CODEKARO partial
+
+try:
+# if(True):
+    
+    def modInverse(a,m) : 
+        m0=m
+        y=0
+        x=1
+        if(m==1): 
+            return 0
+        while(a>1):
+            q=a//m
+            t=m
+            m=a%m
+            a=t
+            t=y
+            y=x-q*y
+            x=t
+        if(x<0): 
+            x=x+m0 
+        return x 
+    
+    
+    for _ in range(int(input())):
+        m=10**9+7
+        n,k=map(int,input().split())
+        for _ in range(n):
+            l=int(input())
+            lll=l//2
+            down=pow(52,lll,m)
+            tot=modInverse(down,m)
+            print(tot)
+except:
+    pass
+
+
+#corona scare partial
+
+try:
+# if(True):
+    for _ in range(int(input())):
+        x=int(input())
+        n=int(input())
+        a=list(map(int,input().split()))
+        m=int(input())
+        b=list(map(int,input().split()))
+            
+        count=0
+        for j in range(m-n+1):
+            dif=b[j]-a[0]
+            if(dif>=0 and dif<=x):
+                flag=True
+                ind=0
+                k=j
+                for _ in range(n):
+                    if(b[k]-dif!=a[ind]):
+                        flag=False
+                        break
+                    ind+=1
+                    k+=1
+                if(flag):
+                    count+=1
+            elif(dif<0 and (-1*dif)<=x):
+                flag=True
+                ind=0
+                k=j
+                for _ in range(n):
+                    if(a[ind]+dif!=b[k]):
+                        flag=False
+                        break
+                    ind+=1
+                    k+=1
+                if(flag):
+                    count+=1
+        print(count)
+    
+except:
+    pass
+
+
+#maximum product multiple (partial)
+
+
+try:
+# if(True):
+    for _ in range(int(input())):
+        a,b,k=map(int,input().split())
+        li=[]
+        fn=[]
+        f=0
+        maxsum=0
+        maxx=0
+        for i in range(a,b+1):
+            if(i%k==0):
+                f=i
+                break
+        if(f==0):
+            print(-1)
+            continue
+        for i in range(f,b+1,k):
+            if(i%10==0):
+                continue
+            else:
+                ss=str(i)
+                flag=False
+                summ=1
+                for j in range(len(ss)-1):
+                                      
+                    if(ss[j]=="0"):
+                        flag=True
+                        break
+                        
+                    iiflag=False
+                    for p in range(j+1,len(ss)):
+                        if(ss[p]=="0"):
+                            break
+                        
+                        if(int(ss[j])%int(ss[p])==0):
+                            iiflag=True
+                            break
+                    if(iiflag==False):
+                        flag=True
+                        break
+                    summ*=int(ss[j])
+                if(flag):
+                    continue
+                else:
+                    summ*=int(ss[-1])
+                    li.append(i)
+                    fn.append(summ)
+                    if(maxsum<summ):
+                        maxsum=summ
+                        maxx=i
+        if(maxx!=0):
+            print(maxsum,maxx)
+        else:
+            print(-1)
+                    
+                    
+    
+    
+    
+except:
+    pass
+
